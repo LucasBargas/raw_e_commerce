e0eacf3112b654dff80b57d18796b6cdef76186b
LucasBargas <lucas.bargasoficial@gmail.com>
2022-05-17 19:54:58 -0300

    Configurando Theme

--- styles/Themes.js
+++ styles/Themes.js
@@ -1,5 +1,11 @@
 const Theme = {
   colors: {
+    primaryColor: '#181818',
+    secondayColor: '#efefef',
+    grayColor: '#2d2d2d',
+    titleGray: '#aaa19b',
+    tomatoColor: '#ff6347',
+    blueColor: '#0000ff',
     lightColor: '#ffffff',
   },
 }
