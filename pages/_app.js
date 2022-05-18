86233e8c67e248444b9e4922742775b5dbae5a9a
LucasBargas <lucas.bargasoficial@gmail.com>
2022-05-17 21:58:14 -0300

    Head da página inicial - em construção

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,13 @@
 import GlobalStyle from '../styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 import Theme from '../styles/Themes';
-import Head from 'next/head';
 
 const App = ({ Component, pageProps }) => {
   return (
-    <>
-      <Head>
-        <title>Projetos Lucas Bargas - Raw</title>
-        <meta name="description" content="Esta aplicação é uma réplica desenvolvida por Lucas Bargas, com o objetivo de praticar o framework NextJS em conjunto com o Styled Components." />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Lucas Bargas - Raw" />
-        <meta property="og:description" content="Esta aplicação é uma réplica desenvolvida por Lucas Bargas, com o objetivo de praticar o framework NextJS em conjunto com o Styled Components." />
-        <meta property="og:url" content="https://raw-plum.vercel.app/" />
-      </Head>
-
-      <ThemeProvider theme={Theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+import Head from "next/head";
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Head>
+        <title>Projetos Lucas Bargas - Raw</title>
+        <meta name="description" content="Esta aplicação é uma réplica desenvolvida por Lucas Bargas, com o objetivo de praticar o framework NextJS em conjunto com o Styled Components." />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Lucas Bargas - Raw" />
+        <meta property="og:description" content="Esta aplicação é uma réplica desenvolvida por Lucas Bargas, com o objetivo de praticar o framework NextJS em conjunto com o Styled Components." />
+        <meta property="og:url" content="https://raw-plum.vercel.app/" />
+      </Head>
+
+      <div>
+        Home
+      </div>
+    </>
   )
 }
 
