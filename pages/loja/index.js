f55522c94bf0571cd054e0aee06c4e785aa60f8a
LucasBargas <lucas.bargasoficial@gmail.com>
2022-05-19 16:38:07 -0300

    Head de cada página - preenchido

--- pages/loja/index.js
+++ pages/loja/index.js
@@ -1,7 +1,18 @@
+import Head from "next/head";
+
 const Shop = () => {
   return (
-    <div>Shop</div>
+   <>
+      <Head>
+        <title>Projetos Lucas Bargas - Raw - Loja</title>
+        <meta name="description" content="Esta aplicação é uma réplica desenvolvida por Lucas Bargas, com o objetivo de praticar o framework NextJS em conjunto com o Styled Components." />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Lucas Bargas - Raw - Loja" />
+        <meta property="og:description" content="Esta aplicação é uma réplica desenvolvida por Lucas Bargas, com o objetivo de praticar o framework NextJS em conjunto com o Styled Components." />
+        <meta property="og:url" content="https://raw-lucasbargas.vercel.app/loja" />
+      </Head>
+   </>
   )
 }
 
-export default Shop
+export default Shop;
--- pages/quem-somos/index.js
+++ pages/quem-somos/index.js
@@ -1,6 +1,17 @@
+import Head from "next/head";
+
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <>
+      <Head>
+        <title>Projetos Lucas Bargas - Raw - Quem Somos</title>
+        <meta name="description" content="Esta aplicação é uma réplica desenvolvida por Lucas Bargas, com o objetivo de praticar o framework NextJS em conjunto com o Styled Components." />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Lucas Bargas - Raw - Quem Somos" />
+        <meta property="og:description" content="Esta aplicação é uma réplica desenvolvida por Lucas Bargas, com o objetivo de praticar o framework NextJS em conjunto com o Styled Components." />
+        <meta property="og:url" content="https://raw-lucasbargas.vercel.app/quem-somos" />
+      </Head>
+    </>
   )
 }
 
